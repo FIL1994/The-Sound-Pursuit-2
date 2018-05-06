393480b828d6b9c39173b228ac9061aed23e73a3
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-05-05 22:27:19 -0400

    move style sheets

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -6,11 +6,9 @@ if exist "build" rmdir "build" /S /Q
 mkdir "build"
 cd "build"
 del *.* /F /Q
-if not exist "style" mkdir "style"
 if not exist "assets" mkdir "assets"
 cd "../"
 For %%a in (bundle.js, index.html) do xcopy %%a build /Y
-xcopy "style" "build/style" /E /F /C /Q /Y
 xcopy "assets" "build/assets" /E /F /C /Q /Y
 :: "c:\Program Files\7-Zip\7z.exe" a -tzip "build/build.zip" "./build/*"
 echo "Build was successful"
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import { NG } from "./ng/UnlockMedals";
 import App from "./components/App";
 import reducers from "./reducers";
 import "./setupSoundJS";
+import "./styles/spectre-dark.min.css";
 import "./index.scss";
 
 const store = createStore(reducers, applyMiddleware(ReduxThunk));
